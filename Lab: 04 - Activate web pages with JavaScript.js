f8be8d1da34fee92b30ace913356420756bc985f
AmeenBassamSaleh <ameenbassam222@gmail.com>
2021-03-06 21:06:09 +0200

    v2 lab 04

---
+++ Lab: 04 - Activate web pages with JavaScript.js
@@ -0,0 +1,27 @@
+
+
+console.log ("Hi evryone")
+var x = prompt("What do you think about Mansaf? like it, did not like");
+
+if(x == "like it")
+document.write ("as we are")
+else
+if (x == "did not like")
+document.write ("we will try to change your mind")
+else alert ("refresh the page and try between (like it, did not like)")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
