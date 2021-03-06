fd53d6a5fafdb0f5942c7c6c2e3b8646c5db6301
AmeenBassamSaleh <ameenbassam222@gmail.com>
2021-03-06 22:10:58 +0200

    try lab04

--- Lab: 04 - Activate web pages with JavaScript.js
+++ Lab: 04 - Activate web pages with JavaScript.js
@@ -2,12 +2,14 @@
 
 console.log ("Hi evryone")
 var x = prompt("What do you think about Mansaf? like it, did not like");
+var a =("<h1> As we are </h1>" )
+var b =("<h1> We will try to change your mind </h1>")
 
 if(x == "like it")
-document.write ("as we are")
+document.write (a)
 else
 if (x == "did not like")
-document.write ("we will try to change your mind")
+document.write (b)
 else alert ("refresh the page and try between (like it, did not like)")
 
 
