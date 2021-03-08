e18e7fb5e96bad13fd74f0aa448341bc5b56e697
AmeenBassamSaleh <ameenbassam222@gmail.com>
2021-03-08 15:22:18 +0200

    lab60a

---
+++ Lab: 06a - Programming with JavaScript.js
@@ -0,0 +1,15 @@
+var b = prompt("Did you like Mansaf?");
+
+var a = Function(b)
+{
+
+     for(i = 0; i <= 3; i++)
+     document.write("HI " + b);
+
+}
+
+//  a("  b")
+
+// function render(){
+
+// }
